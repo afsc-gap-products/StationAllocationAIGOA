769c605978de4f5011e793bc192e3eebdb0f8d02
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-10-20 15:39:31 -0700

    confirmed 2021 areas (see desc.)
    updated 2021 untrawlable areas include
    1) areas associated with historical stratum 0
    2) stations deemed untrawlable as of 2019
    3) additional stations deemed untrawlable in 2021
    
    and excludes stations that were untrawlable in 2019 but were deemed trawlable in 2021 (did not see any stations that were flipped...)

--- analysis_scripts/GOA/get_historical_stations.R
+++ analysis_scripts/GOA/get_historical_stations.R
@@ -3,11 +3,14 @@
 ## Author:        Zack Oyafuso (zack.oyafuso@noaa.gov)
 ## Description:
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## Restart R Session before running
 rm(list = ls())
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Packages
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(terra)
 library(RODBC)
 library(getPass)
 
@@ -41,21 +44,48 @@ goa_strata_2021 <-  RODBC::sqlQuery(channel = channel, query = strata_q)
 untrawl_2019_shp <- terra::vect(x = paste0("data/GOA/shapefiles_from_GDrive/",
                                            "goagrid2019_landuntrawlsndmn.shp"))
 
+stratum_0_IDs <- sort(subset(x = as.data.frame(untrawl_2019_shp),
+                             subset = STRATUM == 0)$GOAGRID_ID)
+
+untrawlable_stations_2019 <- sort(subset(x = as.data.frame(untrawl_2019_shp),
+                                         subset = STRATUM != 0)$GOAGRID_ID)
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Identify untrawlable stations as of 2021
 ##   Create a shapefile of untrawlable stations
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-untrawlable_stations <-
-  na.omit(goa_grid_2021$GOAGRID_ID[goa_grid_2021$TRAWLABLE == "N"])
+untrawlable_stations_2021 <-
+  sort(subset(x = goa_grid_2021, subset = TRAWLABLE == "N")$GOAGRID_ID)
 
 goa_grid_2021_shp <- terra::vect("data/GOA/shapefiles_from_GDrive/goagrid.shp")
+
 untrawl_2021_shp <-
-  goa_grid_2021_shp[goa_grid_2021_shp$GOAGRID_ID %in% untrawlable_stations]
+  goa_grid_2021_shp[goa_grid_2021_shp$GOAGRID_ID %in% untrawlable_stations_2021]
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Merge 2019 shapefile of untrawlable areas and
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-untrawl_2021_shp <- terra::union(untrawl_2019_shp, untrawl_2021_shp)
+untrawl_2021_shp <- terra::union(untrawl_2019_shp[untrawl_2019_shp$STRATUM == 0],
+                                 untrawl_2021_shp)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Plot
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+goa_grid <- terra::vect(x = "data/GOA/shapefiles_from_GDrive/goagrid.shp")
+goa_domain <- terra::aggregate(x = goa_grid)
+
+pdf(file = "data/GOA/processed_shapefiles/UT_areas_2021.pdf",
+    width = 10, height = 8)
+plot(untrawl_2019_shp, col = "cyan", border = F, axes = F)
+plot(untrawl_2021_shp[!untrawl_2021_shp$GOAGRID_ID %in%
+                        c(stratum_0_IDs, untrawlable_stations_2019)],
+     col = "red", add = TRUE, border = F)
+plot(goa_domain, add = TRUE, lwd = 0.5)
+legend("topleft", legend = c("Untrawlable as of 2019,\nLand, and Sandman Reef",
+                             "Added in 2021"),
+       fill = c("cyan", "red"))
+dev.off()
+
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Upload dfs to repo
