6fcaa90cdf701691aead66f3054a0057ab45db71
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-10-25 22:19:09 -0700

    added rest of data variables (not documented yet)

--- R/data.R
+++ R/data.R
@@ -13,3 +13,101 @@
 #' }
 #' @source \url{https://www.ncdc.noaa.gov/}
 "frame"
+
+#' Gulf of Alaska stations as of the 2023 restratification
+#'
+#' @format ## `stations`
+#' A data frame with 25504 rows and 14 columns. Useful columns are:
+#' \describe{
+#'   \item{STATIONID}{ID of the 5 km grid cell to which the station belongs}
+#'   \item{GOAGRID_ID}{unique station ID}
+#'   \item{TRAWLABLE_AREA_KM2}{untrawlable area (km^2)}
+#'   \item{PERIMETER_KM}{perimeter of station (km)}
+#'   \item{STRATUM}{stratum ID}
+#'   \item{CENTER_LAT}{latitude of the station centroid}
+#'   \item{CENTER_LONG}{longitude of the station centroid}
+#'
+#' }
+"stations"
+
+
+#' Gulf of Alaska stations as of the 2023 restratification
+#'
+#' @format ## `ak_land`
+#' A data frame with 25504 rows and 14 columns. Useful columns are:
+#' \describe{
+#'   \item{STATIONID}{ID of the 5 km grid cell to which the station belongs}
+#'   \item{GOAGRID_ID}{unique station ID}
+#'   \item{TRAWLABLE_AREA_KM2}{untrawlable area (km^2)}
+#'   \item{PERIMETER_KM}{perimeter of station (km)}
+#'   \item{STRATUM}{stratum ID}
+#'   \item{CENTER_LAT}{latitude of the station centroid}
+#'   \item{CENTER_LONG}{longitude of the station centroid}
+#'
+#' }
+"ak_land"
+
+#' Gulf of Alaska stations as of the 2023 restratification
+#'
+#' @format ## `ca_land`
+#' A data frame with 25504 rows and 14 columns. Useful columns are:
+#' \describe{
+#'   \item{STATIONID}{ID of the 5 km grid cell to which the station belongs}
+#'   \item{GOAGRID_ID}{unique station ID}
+#'   \item{TRAWLABLE_AREA_KM2}{untrawlable area (km^2)}
+#'   \item{PERIMETER_KM}{perimeter of station (km)}
+#'   \item{STRATUM}{stratum ID}
+#'   \item{CENTER_LAT}{latitude of the station centroid}
+#'   \item{CENTER_LONG}{longitude of the station centroid}
+#'
+#' }
+"ca_land"
+
+#' Gulf of Alaska stations as of the 2023 restratification
+#'
+#' @format ## `strata_list`
+#' A data frame with 25504 rows and 14 columns. Useful columns are:
+#' \describe{
+#'   \item{STATIONID}{ID of the 5 km grid cell to which the station belongs}
+#'   \item{GOAGRID_ID}{unique station ID}
+#'   \item{TRAWLABLE_AREA_KM2}{untrawlable area (km^2)}
+#'   \item{PERIMETER_KM}{perimeter of station (km)}
+#'   \item{STRATUM}{stratum ID}
+#'   \item{CENTER_LAT}{latitude of the station centroid}
+#'   \item{CENTER_LONG}{longitude of the station centroid}
+#'
+#' }
+"strata_list"
+
+#' Gulf of Alaska stations as of the 2023 restratification
+#'
+#' @format ## `depth_mods`
+#' A data frame with 25504 rows and 14 columns. Useful columns are:
+#' \describe{
+#'   \item{STATIONID}{ID of the 5 km grid cell to which the station belongs}
+#'   \item{GOAGRID_ID}{unique station ID}
+#'   \item{TRAWLABLE_AREA_KM2}{untrawlable area (km^2)}
+#'   \item{PERIMETER_KM}{perimeter of station (km)}
+#'   \item{STRATUM}{stratum ID}
+#'   \item{CENTER_LAT}{latitude of the station centroid}
+#'   \item{CENTER_LONG}{longitude of the station centroid}
+#'
+#' }
+"depth_mods"
+
+#' Gulf of Alaska stations as of the 2023 restratification
+#'
+#' @format ## `grid_goa_sp`
+#' A data frame with 25504 rows and 14 columns. Useful columns are:
+#' \describe{
+#'   \item{STATIONID}{ID of the 5 km grid cell to which the station belongs}
+#'   \item{GOAGRID_ID}{unique station ID}
+#'   \item{TRAWLABLE_AREA_KM2}{untrawlable area (km^2)}
+#'   \item{PERIMETER_KM}{perimeter of station (km)}
+#'   \item{STRATUM}{stratum ID}
+#'   \item{CENTER_LAT}{latitude of the station centroid}
+#'   \item{CENTER_LONG}{longitude of the station centroid}
+#'
+#' }
+"grid_goa_sp"
+
