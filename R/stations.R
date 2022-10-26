0890029c9b3fc44690a219fdf142109e2f367bcf
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-10-25 21:49:37 -0700

    added documentation

---
+++ R/stations.R
@@ -0,0 +1,15 @@
+#' Gulf of Alaska stations as of the 2023 restratification
+#'
+#' @format ## `stations`
+#' A data frame with 25504 rows and 14 columns. Useful columns are:
+#' \describe{
+#'   \item{STATIONID}{ID of the 5 km grid cell to which the station belongs}
+#'   \item{GOAGRID_ID}{unique station ID}
+#'   \item{TRAWLABLE_AREA_KM2}{untrawlable area (km^2)}
+#'   \item{PERIMETER_KM}{perimeter of station (km)}
+#'   \item{STRATUM}{stratum ID}
+#'   \item{CENTER_LAT}{latitude of the station centroid}
+#'   \item{CENTER_LONG}{longitude of the station centroid}
+#'
+#' }
+"stations"
