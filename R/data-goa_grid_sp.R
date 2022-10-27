86b1521aa53a2043cc9ddf9f87bbc53fce2e2c7a
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-10-27 13:42:12 -0700

    first init

---
+++ R/data-goa_grid_sp.R
@@ -0,0 +1,13 @@
+#' GOA interpolation grid
+#'
+#' Grid used when fitting VAST models and when
+#'
+#' @format SpatVector points with attributes: \describe{
+#'   \item{ID}{Unique grid ID}
+#'   \item{Area_km2}{Area of grid}
+#'   \item{DEPTH_EFH}{Depth in meters}
+#'   \item{stratum}{stratum name}
+#'   \item{manage_are}{management area}
+#' }
+"goa_grid_sp"
+
