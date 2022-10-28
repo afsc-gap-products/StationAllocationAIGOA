1f3c1cd399fb7a2314a1c838ccfe92d4ae85daea
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-10-27 23:40:10 -0700

    testing ss component

--- R/goa_allocate_stations.R
+++ R/goa_allocate_stations.R
@@ -43,7 +43,7 @@ goa_allocate_stations <-
            vessel_names = c("vessel_1", "vessel_2"),
            output_dir = NULL)
   {
-# return(StationAllocationAIGOA::goa_grid_2023)
+    # return(StationAllocationAIGOA::goa_grid_2023)
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ##   Check that species list matches current species list
     ##   Check that year is an integer
@@ -61,103 +61,104 @@ goa_allocate_stations <-
                    "directory path `output_dir` does not exist, please provide",
                    "a path for output products"))
 
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # ##   Constants
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # spp_idx <- match(species, attributes(frame)$sp )
-    # ns_opt <- length(spp_idx)
-    # n_cells <- nrow(frame)
-    # n_years <- unique(frame$WEIGHT)[1]
-    #
-    # strata_names <- levels(factor(grid_goa_sp@data$STRATUM))
-    #
-    #
-    #
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # ##   Subset frame dataset to the specified species set
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # frame <- frame[, c('domainvalue', "id", "WEIGHT",
-    #                    paste0("Y", spp_idx), paste0("Y", spp_idx, "_SQ_SUM") )]
-    # names(frame)[-1:-3] <- c(paste0("Y", 1:ns_opt),
-    #                          paste0("Y", 1:ns_opt, "_SQ_SUM"))
-    #
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # ##   CVs under SRS: upper CV bounds for MS allocation
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # ## Initiate CVs to be those calculated under SRS
-    # ## buildStrataDF calculates the stratum means and variances, X1 = 1
-    # ##     means to calculate those statics on the whole domain
-    # frame$X1 <- 1
-    # srs_stats <- SamplingStrata::buildStrataDF(dataset = frame)
-    # srs_n <- n
-    # srs_var <- as.matrix(srs_stats[, paste0("S", 1:ns_opt)])^2
-    #
-    # srs_var <- sweep(x = srs_var,
-    #                  MARGIN = 1,
-    #                  STATS = (1 - srs_n / n_cells) / srs_n,
-    #                  FUN = "*")
-    #
-    # srs_cv <- sqrt(srs_var) / srs_stats[, paste0("M", 1:ns_opt)]
-    #
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # ##   Single species CV: lower CV bounds for MS allocation
-    # ##   Set X1 to strata
-    # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # frame$X1 <- as.integer(factor(grid_goa_sp@data$STRATUM))
-    # strs_stats <- SamplingStrata::buildStrataDF(dataset = frame)
-    # strs_stats <- strs_stats[order(as.integer(strs_stats$STRATO)), ]
-    # strs_stats$N <- strs_stats$N / n_years
-    #
-    # ss_cv <- data.frame()
-    # ss_allocations <- matrix(nrow = nrow(strs_stats), ncol = ns_opt,
-    #                          dimnames = list(strata_names, species))
-    #
-    # message("")
-    #
-    # for (ispp in 1:ns_opt) {
-    #   error_df <- data.frame("DOM" = "DOM1",
-    #                          "CV1" = as.numeric(srs_cv[ispp]),
-    #                          "domainvalue"  = 1)
-    #   temp_stratif <- cbind(DOM1 = 1,
-    #                         CENS = 0,
-    #                         strs_stats[, c(names(strs_stats)[1:2],
-    #                                        paste0(c("M", "S"), ispp)) ])
-    #   names(temp_stratif)[-c(1:4)] <- paste0(c("M", "S"), 1)
-    #
-    #   temp_bethel <- SamplingStrata::bethel(
-    #     errors = error_df,
-    #     stratif = temp_stratif,
-    #     realAllocation = T,
-    #     printa = T,
-    #     minnumstrat = 4)
-    #   temp_n <- as.integer(sum(temp_bethel))
-    #   temp_cv <- as.numeric(attributes(temp_bethel)$outcv[, "PLANNED CV "])
-    #
-    #   iter = 1
-    #   while (temp_n != n & iter != max_iter){
-    #     over_under <- temp_n > n
-    #     CV_adj <- ifelse(over_under == TRUE,
-    #                      yes = 1.01,
-    #                      no = 0.99)
-    #
-    #     error_df$CV1 <- temp_cv* CV_adj
-    #     temp_bethel <- SamplingStrata::bethel(stratif = temp_stratif,
-    #                                           errors = error_df,
-    #                                           printa = TRUE,
-    #                                           minnumstrat = min_n_per_stratum)
-    #
-    #     temp_n <- sum(as.numeric(temp_bethel))
-    #     temp_cv <- as.numeric(attributes(temp_bethel)$outcv[, "PLANNED CV "])
-    #
-    #     iter = 1 + iter
-    #   }
-    #
-    #   ss_cv <- rbind(ss_cv, data.frame(species = species[ispp],
-    #                                    ss_cv = temp_cv))
-    #   ss_allocations[, ispp] <- temp_bethel
-    #   message(paste0("Incorporating ", species[ispp]) )
-    # }
-    #
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ##   Constants
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    spp_idx <- match(species,
+                     attributes(StationAllocationAIGOA::frame)$sp )
+    ns_opt <- length(spp_idx)
+    n_cells <- nrow(StationAllocationAIGOA::frame)
+    n_years <- unique(StationAllocationAIGOA::frame$WEIGHT)[1]
+
+    strata_names <- levels(factor(StationAllocationAIGOA::frame$stratum))
+
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ##   Subset frame dataset to the specified species set
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    frame_subdf <-
+      StationAllocationAIGOA::frame[, c('domainvalue', "id", "WEIGHT",
+                                        paste0("Y", spp_idx),
+                                        paste0("Y", spp_idx, "_SQ_SUM") )]
+    names(frame_subdf)[-1:-3] <- c(paste0("Y", 1:ns_opt),
+                                   paste0("Y", 1:ns_opt, "_SQ_SUM"))
+
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ##   CVs under SRS: upper CV bounds for MS allocation
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ## Initiate CVs to be those calculated under SRS
+    ## buildStrataDF calculates the stratum means and variances, X1 = 1
+    ##     means to calculate those statics on the whole domain
+    frame_subdf$X1 <- 1
+    srs_stats <- SamplingStrata::buildStrataDF(dataset = frame_subdf)
+    srs_n <- n
+    srs_var <- as.matrix(srs_stats[, paste0("S", 1:ns_opt)])^2
+
+    srs_var <- sweep(x = srs_var,
+                     MARGIN = 1,
+                     STATS = (1 - srs_n / n_cells) / srs_n,
+                     FUN = "*")
+
+    srs_cv <- sqrt(srs_var) / srs_stats[, paste0("M", 1:ns_opt)]
+
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ##   Single species CV: lower CV bounds for MS allocation
+    ##   Set X1 to strata
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    frame_subdf$X1 <- as.integer(factor(frame_subdf$stratum))
+    strs_stats <- SamplingStrata::buildStrataDF(dataset = frame_subdf)
+    strs_stats <- strs_stats[order(as.integer(strs_stats$STRATO)), ]
+    strs_stats$N <- strs_stats$N / n_years
+
+    ss_cv <- data.frame()
+    ss_allocations <- matrix(nrow = nrow(strs_stats), ncol = ns_opt,
+                             dimnames = list(strata_names, species))
+
+    message("")
+
+    for (ispp in 1:ns_opt) {
+      error_df <- data.frame("DOM" = "DOM1",
+                             "CV1" = as.numeric(srs_cv[ispp]),
+                             "domainvalue"  = 1)
+      temp_stratif <- cbind(DOM1 = 1,
+                            CENS = 0,
+                            strs_stats[, c(names(strs_stats)[1:2],
+                                           paste0(c("M", "S"), ispp)) ])
+      names(temp_stratif)[-c(1:4)] <- paste0(c("M", "S"), 1)
+
+      temp_bethel <- SamplingStrata::bethel(
+        errors = error_df,
+        stratif = temp_stratif,
+        realAllocation = T,
+        printa = T,
+        minnumstrat = 4)
+      temp_n <- as.integer(sum(temp_bethel))
+      temp_cv <- as.numeric(attributes(temp_bethel)$outcv[, "PLANNED CV "])
+
+      iter = 1
+      while (temp_n != n & iter != max_iter){
+        over_under <- temp_n > n
+        CV_adj <- ifelse(over_under == TRUE,
+                         yes = 1.01,
+                         no = 0.99)
+
+        error_df$CV1 <- temp_cv* CV_adj
+        temp_bethel <- SamplingStrata::bethel(stratif = temp_stratif,
+                                              errors = error_df,
+                                              printa = TRUE,
+                                              minnumstrat = min_n_per_stratum)
+
+        temp_n <- sum(as.numeric(temp_bethel))
+        temp_cv <- as.numeric(attributes(temp_bethel)$outcv[, "PLANNED CV "])
+
+        iter = 1 + iter
+      }
+
+      ss_cv <- rbind(ss_cv, data.frame(species = species[ispp],
+                                       ss_cv = temp_cv))
+      ss_allocations[, ispp] <- temp_bethel
+      message(paste0("Incorporating ", species[ispp]) )
+    }
+
     # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # ##   multi species CV:
     # ##   Start at SRS CV --> bethel algorithm --> optimal sampling size (n_srs)
