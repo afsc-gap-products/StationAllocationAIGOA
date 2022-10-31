29f142f3b4ba0819cdb0802fa531a87515dcc6d5
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2022-10-30 21:40:12 -0700

    updated spatial functions

--- analysis_scripts/GOA/goa_restratification_2023/plot_solution_results.R
+++ analysis_scripts/GOA/goa_restratification_2023/plot_solution_results.R
@@ -20,14 +20,21 @@ plot_solution_results <- function (file_name,
   par(mfrow = c(3, 2))
   for (i in 1:5) {
     ireg <- c("Shumagin", "Chirikof", "Kodiak", "Yakutat", "Southeastern")[i]
-    tmp <- terra::mask(x = grid_object, mask = nmfs[nmfs$area_name == ireg])
-    tmp$solution <- as.integer(as.factor(tmp$solution))
+
+    grid_goa_sp <- terra::vect(x = grid_object[grid_object$INPFC_AREA == ireg, ],
+                               geom = c("x", "y"),
+                               crs = attributes(optim_df)$crs)
+
+    # tmp <- terra::mask(x = grid_goa_sp,
+    #                    mask = districts_object[districts_object$area_name == ireg])
+    grid_goa_sp$solution <- as.integer(as.factor(grid_goa_sp$solution))
 
     tmp_str_bound <- subset(strata_bounds, subset = Domain == i)
 
-    plot(tmp, col = RColorBrewer::brewer.pal(n = max(tmp$solution),
-                                             name = "Spectral")[tmp$solution],
-         pch = 15,
+    plot(grid_goa_sp,
+         col = RColorBrewer::brewer.pal(n = max(grid_goa_sp$solution),
+                                        name = "Spectral")[grid_goa_sp$solution],
+         pch = ".",
          axes = F, main = ireg)
     legend(c("Shumagin" = "topleft", "Chirikof" = "topleft",
              "Kodiak" = "bottomright", "Yakutat" = "bottom",
@@ -35,7 +42,7 @@ plot_solution_results <- function (file_name,
            legend = paste0(round(tmp_str_bound$Lower_X1), "-",
                            round(tmp_str_bound$Upper_X1), " m, n = ",
                            tmp_str_bound$Allocation),
-           fill = RColorBrewer::brewer.pal(n = max(tmp$solution),
+           fill = RColorBrewer::brewer.pal(n = max(grid_goa_sp$solution),
                                            name = "Spectral"),
            cex = 0.7)
   }
