0f5f139f0d0ada3bb5644b218b400eb38d144ad5
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-01-18 20:14:00 -0800

    see description
    removed need for frame in downstream scripts, thus removing need for SamplingStrata package

--- analysis_scripts/GOA/1E_optimization_data.R
+++ analysis_scripts/GOA/1E_optimization_data.R
@@ -14,15 +14,11 @@ rm(list = ls())
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Import Library
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-library(devtools)
-devtools::install_github(repo = "zoyafuso-NOAA/SamplingStrata")
-library(SamplingStrata)
 library(terra)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Load the true density, true index, and spatial domain dataset
+##   Load the true density, and spatial domain dataset
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# load(file = "data/GOA/prednll_VAST_models.RData")
 grid_goa <- read.csv(file = "data/GOA/vast_grid_goa.csv")
 D_gct <- readRDS("data/GOA/VAST_fit_D_gct.RDS")
 
@@ -72,49 +68,8 @@ optim_df <- cbind(as.data.frame(grid_goa_sp),
                   terra::geom(grid_goa_sp)[, c("x", "y")])
 attributes(optim_df)$crs <- terra::crs(grid_goa_sp)
 
-##################################################
-####   Our df will have fields for:
-####   domain: only one domain so the value is just 1
-####   id: unique ID for each sampling cell
-####   X1: strata variable 2: depth of cell (m)
-####   X2: strata variable 1: longitude in eastings (km). Because the
-####       optimization does not read in negative values, I shift the
-####       values so that the lowest value is 0
-####
-####   Variables used to more efficiently calcualte stratum variance
-####
-####   WEIGHT: number of observed years
-####   Y1, Y2, ... : density for a given cell summed across observed years
-####   Y1_SQ_SUM, Y2_SQ_SUM, ... : density-squared for a given cell,
-####           summed across observed years
-##################################################
-frame <- cbind(
-  data.frame(domainvalue = 1,
-             id = 1:n_cells,
-             depth = grid_goa_sp$DEPTH_EFH,
-             eastings_m = geom(grid_goa_sp)[, "x"],
-             northings_m = geom(grid_goa_sp)[, "y"],
-             stratum = grid_goa_sp$STRATUM[],
-             WEIGHT = n_years),
-
-  matrix(data = apply(X = D_gct,
-                      MARGIN = 1:2,
-                      FUN = sum),
-         ncol = n_spp,
-         dimnames = list(NULL, paste0("Y", 1:n_spp))),
-
-  matrix(data = apply(X = D_gct,
-                      MARGIN = 1:2,
-                      FUN = function(x) sum(x^2)),
-         ncol = n_spp,
-         dimnames = list(NULL, paste0("Y", 1:n_spp, "_SQ_SUM")))
-)
-
-attributes(frame)$spp_name <- dimnames(D_gct)[[2]]
-
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Save Data
+##   Save data internally
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 usethis::use_data(D_gct, overwrite = TRUE)
 usethis::use_data(optim_df, overwrite = TRUE)
-# usethis::use_data(frame, overwrite = TRUE)
