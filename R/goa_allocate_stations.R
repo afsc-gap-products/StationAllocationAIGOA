c77f3a6296d3f8b3efaa8da16535ac54ec8d8a3f
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-01-18 20:06:44 -0800

    see description
    - updated year names from 2023 to 2025
    - calculations of design-based statistics are now calculated directly instead of using the the SamplingStrata package
    - SamplingStrata package is only used for the bethel() functions
    -added a max_iter argument to terminate the optimization procedure if it can't asymptote to a particular sample size.
    - added a trawl argument to denote which types of stations you want to include (default is "Y" and "UNK" trawlable stations)

--- R/goa_allocate_stations.R
+++ R/goa_allocate_stations.R
@@ -16,7 +16,6 @@
 #'                 to converge to `n`.
 #' @param year integer. The year as an integer is used as a seed for the random
 #'             number generator when drawing random stations.
-#' @param vessel_names character vector.
 #'
 #' @return A named list with elements:
 #' 1) ms_allocation: vector, allocation of `n` stations across strata using
@@ -24,30 +23,12 @@
 #' 2) drawn_stations: dataframe with `n` row and 15 fields. Notable fields are:
 #' \describe{
 #' \item{STATIONID}{Station ID of the grid the station resides. This format of the station id is XX-YY where XX is the grid location on the W-E axis and YY is the grid location on the N-S axis.}
-#' \item{STRATUM}{Stratum ID. The 2023 restratification stratum id is formatted as the year the stratification was created (i.e., 2023), followed by "_" and then a three digit code in the format ABC similar to the format used in the historical GOA survey design. The second digit (B) refers to the managment area (1: Shumagin, 2: Chirikof, 3: Kodiak, 4: Yakutat, and 5: Southeastern). The first digit (A) denotes the relative depth of the stratum, 0 being the shallowest stratum and 5 being the deepest stratum. Because each management area has different depth strata, this digit is not comparable among management areas, just within a management area, e.g., 2023_010 is shallower than 2023_110. The third digit C is a replicate counter in case there are multiple strata within a management area x depth bin combination (there are no replicates in the 2023 GOA stratification, so this digit is always zero). This last digit is consistent with the historical GOA strata ids. }
+#' \item{STRATUM}{Stratum ID. The 2025 restratification stratum id is formatted as the year the stratification was created (i.e., 2025), followed by "_" and then a three digit code in the format ABC similar to the format used in the historical GOA survey design. The second digit (B) refers to the managment area (1: Shumagin, 2: Chirikof, 3: Kodiak, 4: Yakutat, and 5: Southeastern). The first digit (A) denotes the relative depth of the stratum, 0 being the shallowest stratum and 5 being the deepest stratum. Because each management area has different depth strata, this digit is not comparable among management areas, just within a management area, e.g., 2023_010 is shallower than 2023_110. The third digit C is a replicate counter in case there are multiple strata within a management area x depth bin combination (there are no replicates in the 2023 GOA stratification, so this digit is always zero). This last digit is consistent with the historical GOA strata ids. }
 #' \item{AREA_KM2}{area of station in km^2}
 #' \item{TRAWLABLE_AREA_KM2}{trawlable area within station in km^2}
 #' \item{PERIMETER_KM}{perimeter of station in km}
 #' \item{CENTER_LAT, CENTER_LONG}{Latitude and longitude of station centroid}
-#' \item{vessel}{vessel name assigned to station}
 #' }
-#'
-#'@examples
-#'
-#' spp_list <- c("walleye pollock", "Pacific cod", "arrowtooth flounder",
-#' "flathead sole", "rex sole", "northern rock sole",
-#' "southern rock sole", "Dover sole", "Pacific halibut",
-#' "Pacific ocean perch", "BS and RE rockfishes",
-#' "silvergray rockfish", "dusky rockfish", "northern rockfish",
-#' "shortspine thornyhead")
-#'
-#' GOA_allocation_2023 <-
-#'   StationAllocationAIGOA::goa_allocate_stations(
-#'     species = spp_list,
-#'     n = 550,
-#'     min_n_per_stratum = 4,
-#'     year = 2023,
-#'     vessel_names = c("vessel_1",  "vessel_2"))
 #'
 
 goa_allocate_stations <-
@@ -70,15 +51,13 @@ goa_allocate_stations <-
                        "shortspine thornyhead" ## Sebastolobus alascanus)
            ),
            max_iter = 5000,
-           year = 2023,
-           vessel_names = c("vessel_1", "vessel_2"))
-  {
+           year = 2025){
 
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ##   Check that species list matches current species list
     ##   Check that year is an integer
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    if (!all(species %in% attributes(StationAllocationAIGOA::frame)$sp))
+    if (!all(species %in% dimnames(StationAllocationAIGOA::D_gct)[[2]]))
       stop(paste("message from StationAllocationAIGOA::goa_allocate_stations:",
                  "Argument `species` contains names that are not currently",
                  "in the list of included species. See",
@@ -88,57 +67,64 @@ goa_allocate_stations <-
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ##   Constants
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    spp_idx <- match(species,
-                     attributes(StationAllocationAIGOA::frame)$sp )
+    dens <- StationAllocationAIGOA::D_gct
+    spp_idx <- match(species, dimnames(dens)[[2]] )
     ns_opt <- length(spp_idx)
-    n_cells <- nrow(StationAllocationAIGOA::frame)
-    n_years <- unique(StationAllocationAIGOA::frame$WEIGHT)[1]
+    n_cells <- dim(dens)[1]
+    n_years <- dim(dens)[3]
 
-    strata_names <- levels(factor(StationAllocationAIGOA::frame$stratum))
-
+    strata_names <- with(StationAllocationAIGOA::depth_mods_2023,
+                         stratum[used])
+    NMFS_area <- with(StationAllocationAIGOA::depth_mods_2023,
+                      manage_area[used])
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ##   Subset frame dataset to the specified species set
+    ##   Single species CV: lower CV bounds for MS allocation
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    frame_subdf <-
-      StationAllocationAIGOA::frame[, c('domainvalue', "id", "WEIGHT",
-                                        "stratum",
-                                        paste0("Y", spp_idx),
-                                        paste0("Y", spp_idx, "_SQ_SUM") )]
-    names(frame_subdf)[-1:-4] <- c(paste0("Y", 1:ns_opt),
-                                   paste0("Y", 1:ns_opt, "_SQ_SUM"))
-
-    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ##   CVs under SRS: upper CV bounds for MS allocation
-    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ## Initiate CVs to be those calculated under SRS
-    ## buildStrataDF calculates the stratum means and variances, X1 = 1
-    ##     means to calculate those statics on the whole domain
-    frame_subdf$X1 <- 1
-    srs_stats <- SamplingStrata::buildStrataDF(dataset = frame_subdf)
-    srs_n <- n
-    srs_var <- as.matrix(srs_stats[, paste0("S", 1:ns_opt)])^2
-
-    srs_var <- sweep(x = srs_var,
+    srs_var <- apply(X = dens[, spp_idx, ],
+                     MARGIN = 2,
+                     FUN = function(x) var(as.vector(x)))
+    srs_var <- sweep(x = as.matrix(srs_var),
                      MARGIN = 1,
-                     STATS = (1 - srs_n / n_cells) / srs_n,
+                     STATS = (1 - n / n_cells) / n,
                      FUN = "*")
-
-    srs_cv <- sqrt(srs_var) / srs_stats[, paste0("M", 1:ns_opt)]
+    srs_mean <- apply(X = dens[, spp_idx, ],
+                      MARGIN = 2,
+                      FUN = function(x) mean(as.vector(x)))
+    srs_cv <- sqrt(srs_var) / srs_mean
 
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ##   Single species CV: lower CV bounds for MS allocation
-    ##   Set X1 to strata
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    frame_subdf$X1 <- as.integer(factor(frame_subdf$stratum))
-    strs_stats <- SamplingStrata::buildStrataDF(dataset = frame_subdf)
-    strs_stats <- strs_stats[order(as.integer(strs_stats$STRATO)), ]
-    strs_stats$N <- strs_stats$N / n_years
+    strs_sd <- apply(X = dens[, spp_idx, ],
+                     MARGIN = 2,
+                     FUN = function(x)
+                       sapply(X = split(x = x,
+                                        f = optim_df$STRATUM),
+                              FUN = function(xx) sd(as.vector(xx))))
+    strs_mean <- apply(X = dens[, spp_idx, ],
+                       MARGIN = 2,
+                       FUN = function(x)
+                         sapply(X = split(x = x,
+                                          f = optim_df$STRATUM),
+                                FUN = function(xx) mean(as.vector(xx))))
 
+    strs_stats <- cbind(data.frame(
+      STRATO = 1:length(strata_names),
+      N = as.numeric(table(optim_df$STRATUM)),
+      COST = 1, CENS = 0, DOM1 = 1,
+      X1 = 1:length(strata_names)#,
+      # row.names = strata_names
+    ),
+    matrix(data = as.vector(strs_mean), ncol = ns_opt,
+           dimnames = list(NULL, paste0("M", 1:ns_opt))),
+    matrix(data = as.vector(strs_sd), ncol = ns_opt,
+           dimnames = list(NULL, paste0("S", 1:ns_opt))) )
+
+    ## Data objects for ss allocations
     ss_cv <- data.frame()
-    ss_allocations <- matrix(nrow = nrow(strs_stats), ncol = ns_opt,
+    ss_allocations <- matrix(nrow = length(strata_names),
+                             ncol = ns_opt,
                              dimnames = list(strata_names, species))
-
-    message("")
 
     for (ispp in 1:ns_opt) { ## Loop over species -- start
 
@@ -182,12 +168,20 @@ goa_allocate_stations <-
         iter = 1 + iter
       }
 
+      ## If max_iter it hit and temp_n != n, shoot out a warning
+      if (temp_n != n & iter != max_iter)
+        warning(paste0("Maximum iteration has been reached for single-species ",
+                       "allocation of ", species[ispp], ". Total stations ",
+                       "allocated may not be equal to ", n,
+                       ". Consider increasing the `max_iter` ",
+                       "argument in goa_allocate_stations()."))
+
       ## record cv and station allocation
       ss_cv <- rbind(ss_cv, data.frame(species = species[ispp],
                                        ss_cv = temp_cv))
       ss_allocations[, ispp] <- temp_bethel
       message(paste0("Incorporating ", species[ispp]) )
-    } ## Loop over species -- start
+    } ## Loop over species -- end
 
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ##   multi species CV:
@@ -198,15 +192,16 @@ goa_allocate_stations <-
     message("Now running multispecies allocation" )
 
     ## set up bethel algorithm @ cv under srs
-    error_df <- cbind("DOM" = "DOM1",
-                      srs_cv,
-                      "domainvalue"  = 1)
-    names(error_df)[2:(1 + ns_opt)] <- paste0("CV", 1:ns_opt)
+    error_df <- cbind(data.frame("DOM" = "DOM1"),
+                      matrix(srs_cv, nrow = 1,
+                             dimnames = list(NULL, paste0("CV", 1:ns_opt))),
+                      data.frame("domainvalue"  = 1))
 
     temp_stratif <- cbind(DOM1 = 1,
                           CENS = 0,
-                          strs_stats[, c(names(strs_stats)[1:2],
-                                         paste0("M", 1:ns_opt),
+                          STRATO = strs_stats$STRATO,
+                          N = strs_stats$N,
+                          strs_stats[, c(paste0("M", 1:ns_opt),
                                          paste0("S", 1:ns_opt)) ])
 
     ## Run initial allocation and record the total effort and cv
@@ -242,13 +237,22 @@ goa_allocate_stations <-
       iter <- iter + 1
     }
 
+    ## If max_iter it hit and temp_n != n, shoot out a warning
+    if (temp_n != n & iter != max_iter)
+      warning(paste0("Maximum iteration has been reached for ",
+                     "MS allocation. Total stations allocated may not be ",
+                     "equal to ", n, ". Consider increasing the `max_iter` ",
+                     "argument in goa_allocate_stations()."))
+
     ## Record Multispcies allocation and cvs
     ms_cv <- as.numeric(updated_cv_constraint)
     ms_allocation <- as.integer(ceiling(temp_bethel))
-    names(ms_allocation) <-
-      levels(factor(StationAllocationAIGOA::frame$stratum))
+    names(ms_allocation) <- rownames(strs_sd)
+    ms_allocation <- ms_allocation[strata_names]
 
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ## Randomly drawn stations
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     drawn_stations <- c()
 
     for (i in 1:length(ms_allocation)) { ## Loop over strata -- start
@@ -260,8 +264,10 @@ goa_allocate_stations <-
       ## available stations are those that are trawlable and > 5 km^2
       available_stations <- with(stations_2023,
                                  which(STRATUM == istratum &
-                                         TRAWLABLE == T &
-                                         AREA_KM2 >= 5.00))
+                                         TRAWLABLE != "N" &
+                                         AREA_KM2 >= 5.00 |
+                                         TRAWLABLE == "N" &
+                                         TRAWLABLE_AREA_KM2 >= 5.00))
       temp_samples <- sample(x = available_stations,
                              size = ms_allocation[i],
                              prob = stations_2023$AREA_KM2[available_stations],
@@ -272,12 +278,8 @@ goa_allocate_stations <-
     drawn_stations <- stations_2023[drawn_stations, ]
     drawn_stations$vessel <- vessel_names
 
-    ## Expected CVs
-    # expected_cvs <- cbind( t(srs_cv), ss_cv$ss_cv, ms_cv )
-    # dimnames(expected_cvs) <- list(species, c("srs_cv", "ss_cv", "ms_cv"))
-
-    return(list(#ss_allocations = ss_allocations,
-                ms_allocation = ms_allocation,
-                #expected_cvs = as.data.frame(expected_cvs),
+    return(list(ms_allocation = data.frame(stratum = strata_names,
+                                           nmfs_area = NMFS_area,
+                                           ms_allocation = ms_allocation),
                 drawn_stations = drawn_stations))
   }
