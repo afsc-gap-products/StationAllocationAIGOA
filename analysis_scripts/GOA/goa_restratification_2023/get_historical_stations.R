e1215040b91099cbfdd2963f828d4d4a3a5159d9
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-01-18 20:18:27 -0800

    moved trawlability section to new script

--- analysis_scripts/GOA/goa_restratification_2023/get_historical_stations.R
+++ analysis_scripts/GOA/goa_restratification_2023/get_historical_stations.R
@@ -1,7 +1,8 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Project:
 ## Author:        Zack Oyafuso (zack.oyafuso@noaa.gov)
-## Description:
+## Description:   Take 2021 stations polygons and assign trawlable information
+##                based on information in GOA.GOAGRID_GIS from oracle
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## Restart R Session before running
@@ -25,77 +26,21 @@ assign(x = "channel",
        envir = .GlobalEnv)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Query Station data from Oracle
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-grid_q <- "select * from GOA.GOAGRID_GIS"
-goa_grid_2021 <-  RODBC::sqlQuery(channel = channel, query = grid_q)
-attributes(goa_grid_2021)$date.accessed <- Sys.Date()
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Query Strata data from Oracle
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 strata_q <- "select * from GOA.GOA_STRATA where SURVEY = 'GOA'"
 goa_strata_2021 <-  RODBC::sqlQuery(channel = channel, query = strata_q)
+attributes(goa_strata_2021)$date.accessed <- Sys.Date()
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Import shapefile of untrawlable areas as of 2019, Land, and sandman reef
-##   that potentially coincide with the goa grid
+##   Import shapefile of historical stations
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-untrawl_2019_shp <- terra::vect(x = paste0("data/GOA/shapefiles_from_GDrive/",
-                                           "goagrid2019_landuntrawlsndmn.shp"))
-
-stratum_0_IDs <- sort(subset(x = as.data.frame(untrawl_2019_shp),
-                             subset = STRATUM == 0)$GOAGRID_ID)
-
-untrawlable_stations_2019 <- sort(subset(x = as.data.frame(untrawl_2019_shp),
-                                         subset = STRATUM != 0)$GOAGRID_ID)
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Identify untrawlable stations as of 2021
-##   Create a shapefile of untrawlable stations
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-untrawlable_stations_2021 <-
-  sort(subset(x = goa_grid_2021, subset = TRAWLABLE == "N")$GOAGRID_ID)
-
 goa_grid_2021_shp <- terra::vect("data/GOA/shapefiles_from_GDrive/goagrid.shp")
-
-untrawl_2021_shp <-
-  goa_grid_2021_shp[goa_grid_2021_shp$GOAGRID_ID %in% untrawlable_stations_2021]
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Merge 2019 shapefile of untrawlable areas and
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-untrawl_2021_shp <- terra::union(untrawl_2019_shp[untrawl_2019_shp$STRATUM == 0],
-                                 untrawl_2021_shp)
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Plot
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-goa_grid <- terra::vect(x = "data/GOA/shapefiles_from_GDrive/goagrid.shp")
-goa_domain <- terra::aggregate(x = goa_grid)
-
-pdf(file = "data/GOA/processed_shapefiles/UT_areas_2021.pdf",
-    width = 10, height = 8)
-plot(untrawl_2019_shp, col = "cyan", border = F, axes = F)
-plot(untrawl_2021_shp[!untrawl_2021_shp$GOAGRID_ID %in%
-                        c(stratum_0_IDs, untrawlable_stations_2019)],
-     col = "red", add = TRUE, border = F)
-plot(goa_domain, add = TRUE, lwd = 0.5)
-legend("topleft", legend = c("Untrawlable as of 2019,\nLand, and Sandman Reef",
-                             "Added in 2021"),
-       fill = c("cyan", "red"))
-dev.off()
-
+goa_grid_df <- as.data.frame(goa_grid_2021_shp)
+attributes(goa_grid_df)$date.accessed <- Sys.Date()
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Upload dfs to repo
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-saveRDS(goa_grid_2021, file = "data/GOA/grid_goa_2021.rds")
+saveRDS(as.data.frame(goa_grid_2021_shp), file = "data/GOA/grid_goa_2021.rds")
 saveRDS(goa_strata_2021, file = "data/GOA/grid_strata_2021.rds")
-
-if (!dir.exists("data/GOA/processed_shapefiles/"))
-  dir.create("data/GOA/processed_shapefiles/")
-terra::writeVector(x = untrawl_2021_shp,
-                   filename = paste0("data/GOA/processed_shapefiles/",
-                                     "GOA_untrawl_2021.shp"),
-                   overwrite=TRUE)
--- analysis_scripts/GOA/goa_restratification_2023/goa_strata_adjustments.R
+++ analysis_scripts/GOA/goa_restratification_2023/goa_strata_adjustments.R
@@ -5,6 +5,10 @@
 ##                strata polygons and intersect its boundaries with the 5 km
 ##                GOA grid. Then within each 5km grid cell, calculate the
 ##                total area and perimeter of the stratum component.
+##
+##                When importing and manipulating spatial objects, make sure
+##                the projection is aligned with the bathymetry raster for
+##                consistency.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## Restart R Session before running
@@ -17,10 +21,10 @@ library(terra)
 library(RColorBrewer)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Load depth modifications ----
+##   Import stratum depth boundaries ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 depth_mods <-
-  read.csv("data/GOA/strata_boundaries/depth_modifications_2023.csv")
+  read.csv(file = "data/GOA/strata_boundaries/depth_modifications_2023.csv")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Merge bathy rasters ----
@@ -40,21 +44,14 @@ bathy <- do.call(what = terra::merge, args = split_bathy)
 rm(split_bathy, i, n_split_rasters)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Import Data Polygons ----
-##   goa_grid: historical survey stations
-##   goa_domain: is a mask of the survey footprint made by dissolving the
-##               inner boundaries of the goa_grid
-##   goa_domain_latlon: goa_domain projected onto lon/lat
-##
-##   goa_grid_2021: historical stations from Oracle (GOA.GOAGRID_GIS)
-##   goa_strata_2021: historical strata from Oracle (GOA.GOA_STRATA)
-##
-##   ak_land and ca_land are land polygons for Alaska and Canada
-##   goa_grid_untrawl is a polygon of untrawlable areas
-##
-##   nmfs are polygons of the five management areas
+##   Import 2021 stations ----
+##   First make sure the `goa_grid` is in the same projection as the `bathy`
+##   raster. Next, dissolve the inner bondaries of the `goa_grid` polygons
+##   to get an outline of the GOA domain (`goa_domain`). Then, reproject
+##   `goa_domain` to latlon.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 goa_grid <- terra::vect(x = "data/GOA/shapefiles_from_GDrive/goagrid.shp")
+goa_grid <- terra::project(x = goa_grid, y = bathy)
 goa_domain <- terra::aggregate(x = goa_grid)
 
 latlon_crs <-
@@ -62,10 +59,9 @@ latlon_crs <-
                                         "GOAdissolved.shp")))
 goa_domain_latlon <- terra::project(x = goa_domain, y = latlon_crs)
 
-goa_grid_2021 <- readRDS(file = "data/GOA/grid_goa_2021.rds")
-goa_grid_2021 <- goa_grid_2021[order(goa_grid_2021$GOAGRID_ID), ]
-goa_strata_2021 <- readRDS(file = "data/GOA/grid_strata_2021.rds")
-
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Import Land Polys ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ak_land <- terra::vect(x = "data/GOA/shapefiles_from_GDrive/AKland.shp")
 ak_land <- terra::project(x = ak_land, y = bathy)
 ak_land <- terra::aggregate(x = ak_land)
@@ -77,28 +73,32 @@ ca_land <- ca_land[ca_land$POPYADMIN %in% c("BRITISH COLUMBIA",
 ca_land <- terra::project(x = ca_land, y = bathy)
 ca_land <- terra::aggregate(x = ca_land)
 
-goa_grid_untrawl <-
-  terra::vect(x = "data/GOA/processed_shapefiles/GOA_untrawl_2021.shp")
-
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Import NMFS Areas ----
+##   Management area is also a stratum variable. Reproject to the same
+##   projection as the `bathy` raster. NMFS statistical areas 650 and 659 are
+##   part of the Southeastern section of the goa domain, so we aggregate those
+##   two areas together.
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 nmfs <- terra::vect(x = "data/GOA/shapefiles_from_GDrive/GOA_Shapes.shp")
 nmfs <- terra::project(x = nmfs, y = bathy)
 nmfs$area_name <- c("Southeastern", "Southeastern", "Shumagin", "Chirikof",
-               "Kodiak", "Yakutat", NA)
+                    "Kodiak", "Yakutat", NA)
 nmfs <- terra::aggregate(x = nmfs, by = "area_name")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Create strata polygons ----
+##   Create stratum polygons ----
 ##   For each management area, create new strata based on depth specifications
 ##   and append to strata_list
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 strata_list <- list()
 for (idistrict in unique(depth_mods$manage_area)) { ## Loop over district --st.
 
-  ## Crop bathymetry raster to just the management area
+  ## Mask bathymetry raster to just the management area and goa_domain
   district_outline <- nmfs[nmfs$area_name == idistrict]
 
-  district_bathy <- terra::crop(x = bathy,
-                                y = district_outline)
+  district_bathy <- terra::mask(x = bathy,
+                                mask = district_outline)
   district_bathy <- terra::mask(x = district_bathy,
                                 mask = goa_domain)
 
@@ -107,19 +107,22 @@ for (idistrict in unique(depth_mods$manage_area)) { ## Loop over district --st.
                            subset = manage_area == idistrict,
                            select = c("lower_depth_m", "upper_depth_m"))
 
-  ## Define each raster cell based on the stratum depth boundaries
+  ## Discretize the `bathy`` raster: Define each raster cell based on the
+  ## defined stratum depth boundaries in `depth_mods` and create an arbitrary
+  ## integer label for each stratum.
   values(district_bathy) <-
     as.integer(as.character(cut(x = values(district_bathy),
                                 breaks = c(0, depth_boundary$upper_depth_m),
                                 labels = 1:nrow(depth_boundary)) ))
 
-  ## Convert raster to polygon
+  ## Convert discretized raster to polygon based on those discrete values
   strata_poly <- terra::as.polygons(district_bathy)
 
   ## Create dataframe of stratum information
   strata_poly[, names(depth_mods)] <-
-      subset(x = depth_mods, subset = manage_area == idistrict)
+    subset(x = depth_mods, subset = manage_area == idistrict)
 
+  ## Calculate the total area and perimeter of the strata.
   strata_poly$AREA_KM2 <- terra::expanse(x = strata_poly, unit = "km")
   strata_poly$PER_KM <- terra::perim(x = strata_poly) / 1000
 
@@ -130,13 +133,12 @@ for (idistrict in unique(depth_mods$manage_area)) { ## Loop over district --st.
 } ## Loop over district -- end
 rm(idistrict, strata_poly, district_outline, district_bathy)
 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Merge strata into one object
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 strata_list <- do.call(what = rbind, args = strata_list)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Take the current goa grid and merge all historical stations wihtin 5x5 km
+##   Create 5x5 km survey grid ----
+##   Take the goa grid and merge all historical stations wihtin 5x5 km
 ##   cell. This essentially resets the stratum data in each grid cell.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 goa_full_grid <- terra::aggregate(x = goa_grid,
@@ -146,18 +148,16 @@ goa_full_grid <- goa_full_grid[, c("ID", "agg_AREA_KM2")]
 names(goa_full_grid) <- c("ID", "AREA_KM2")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   New stations ----
-##
-##   The station ID is a pair of numbers that indicate the position of a grid
-##   cell. The first number indicates the
-##
-##   Intersect the new strata polygons with the 5x5 km grid. This function
-##   takes a few minutes. Then calculate the area and perimeter of each station
+##   Create new stations ----
+##   Intersect the new stratum polygons with the 5x5 km grid to create new
+##   station polygons. This function takes a few minutes. Then calculate the
+##   area, perimeter, and location of each new station.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-stations <- terra::intersect(x = goa_full_grid, y = strata_list)
+stations <- terra::intersect(x = goa_full_grid,
+                             y = strata_list[, c("manage_area", "stratum")])
+
 stations$PER_KM <- terra::perim(x = stations) / 1e3
 stations$AREA_KM2 <- terra::expanse(x = stations) / 1e6
-stations <- terra::project(x = stations, y = bathy)
 
 stations[, c("E_m", "N_m")] <-
   terra::geom(terra::centroids(x = stations, inside = TRUE))[, c("x", "y")]
@@ -167,53 +167,14 @@ stations[, c("Lon", "Lat")] <-
                                inside = TRUE))[, c("x", "y")]
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Untrawlable area calculation ----
-##   Intersect the untrawlable areas with the new stations and calculate
-##   the area of the untrawlable area in the stations where there are
-##   untrawlable areas
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-overlap_with_trawl_polygon <- terra::intersect(stations, goa_grid_untrawl)
-overlap_with_trawl_polygon <- overlap_with_trawl_polygon[, c(1:3, 5:6)]
-overlap_with_trawl_polygon$untrawl_area_km2 <-
-  terra::expanse(x = overlap_with_trawl_polygon) / 1e6
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##  Update stations shapefile with untrawlable information
-##  Create a trawlable field in the stations
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-stations$trawlable <- TRUE
-stations$untrawl_area_km2 <- 0
-
-temp_overlap_df <- as.data.frame(overlap_with_trawl_polygon)
-temp_stations_df <- as.data.frame(stations)
-
-temp_idx <- sapply(X = 1:length(overlap_with_trawl_polygon),
-                   FUN = function(x) {
-                     temp_id <- temp_overlap_df$ID[x]
-                     temp_str <-  temp_overlap_df$stratum[x]
-                     temp_idx <- which(temp_stations_df$ID == temp_id &
-                                         temp_stations_df$stratum == temp_str)
-                     return(temp_idx)
-                   })
-
-stations$trawlable[temp_idx] <- FALSE
-stations$untrawl_area_km2[temp_idx] <- temp_overlap_df$untrawl_area_km2
-stations$trawl_area_km2 <-
-  round(x = stations$AREA_KM2 - stations$untrawl_area_km2, digits = 6)
-
-rm(temp_overlap_df, temp_stations_df, temp_idx)
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Format stations
+##   Format stations ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 goagrid_ids <-
-  (max(goa_grid_2021$GOAGRID_ID) + 1):
-  (max(goa_grid_2021$GOAGRID_ID) + nrow(stations))
+  (max(goa_grid$GOAGRID_ID) + 1):
+  (max(goa_grid$GOAGRID_ID) +  nrow(stations))
 
-stations_2023 <- data.frame("GOAGRID#" = goagrid_ids + 1,
+stations_2023 <- data.frame("GOAGRID#" = goagrid_ids,
                             "GOAGRID_ID" = goagrid_ids,
-                            "TRAWLABLE" = stations$trawlable,
-                            "TRAWLABLE_AREA_KM2" = stations$trawl_area_km2,
                             "AREA_KM2" = round(stations$AREA_KM2, 6),
                             "PERIMETER_KM" = stations$PER_KM,
                             "STRATUM" = stations$stratum,
@@ -222,22 +183,21 @@ stations_2023 <- data.frame("GOAGRID#" = goagrid_ids + 1,
                             "CENTER_LONG" = stations$Lon,
                             check.names = FALSE)
 
-stations_2023[, c("NORTH_LAT", "SOUTH_LAT", "EAST_LONG", "WEST_LONG")] <-
-  goa_grid_2021[match(stations_2023$STATIONID, goa_grid_2021$STATIONID),
-                c("NORTH_LAT", "SOUTH_LAT", "EAST_LONG", "WEST_LONG")]
-
 stations[, names(stations_2023)] <- stations_2023
 stations[, !names(stations) %in% names(stations_2023)] <- NULL
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Which parts of the goa grid are not supported by the bathymetry raster
+##   Areas not defined by bathy ----
+##   Which parts of the goa grid are not supported by the bathymetry raster?
+##   This is mostly for plotting purposes, still need to figure out how to
+##   fill out these parts of the goa domain.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-leftovers <- terra::erase(x = goa_grid, y = stations)
-leftovers[, "unk_area_km2"] <- terra::expanse(x = leftovers) / 1e6
-leftovers[, !names(leftovers) %in% c("ID", "STRATUM", "unk_area_km2")] <- NULL
+# leftovers <- terra::erase(x = goa_grid, y = stations)
+# leftovers[, "unk_area_km2"] <- terra::expanse(x = leftovers) / 1e6
+# leftovers[, !names(leftovers) %in% c("ID", "STRATUM", "unk_area_km2")] <- NULL
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Format strata
+##   Format strata ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 goa_strata_2023 <-
   data.frame("SURVEY" = "GOA",
@@ -265,7 +225,7 @@ strata_list[, names(goa_strata_2023)] <- goa_strata_2023
 strata_list[, !names(strata_list) %in% names(goa_strata_2023)] <- NULL
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##  Save results ----
+##  Save shapefiles ----
 ##  Create export directory and export
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if(!dir.exists("data/GOA/processed_shapefiles/"))
@@ -281,16 +241,16 @@ terra::writeVector(x = stations,
                    overwrite = TRUE)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Save internally
+##   Save internal variables ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-usethis::use_data(stations_2023, overwrite = TRUE, internal = F)
+# usethis::use_data(stations_2023, overwrite = TRUE, internal = F)
 aea_crs <- terra::crs(bathy)
 usethis::use_data(aea_crs, overwrite = TRUE)
 depth_mods_2023 <- depth_mods
 usethis::use_data(depth_mods_2023, overwrite = TRUE)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Plots
+##   Plot, finally ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 pdf(file = "data/GOA/processed_shapefiles/updated_strata.pdf",
@@ -303,30 +263,30 @@ for (iarea in unique(depth_mods$manage_area)) { ## Loop over area -- start
   temp_area <- terra::mask(x = stations, mask = nmfs[nmfs$area_name == iarea] )
 
   ## Base layer
-  plot(temp_area, axes = F, col = "white", border = F)
+  plot(temp_area, axes = F, col = "white", border = F, mar = c(0,0,0,0))
 
   ## Plot stations color coded by strata
   for (istratum in 1:n_strata) {
     plot(stations[stations$STRATUM == temp_strata[istratum]],
-         col =   c(RColorBrewer::brewer.pal("Spectral",
+         col =   c(RColorBrewer::brewer.pal("Set1",
                                             n = n_strata - 1),
-                   "purple")[istratum],
+                   "cyan")[istratum],
          border = F, add = TRUE)
   }
 
   ## Plot areas not covered by bathymery
-  plot(terra::mask(x = leftovers,
-                   mask = nmfs[nmfs$area_name == iarea]),
-       col = "black", add = TRUE, border = F)
+  # plot(terra::mask(x = leftovers,
+  #                  mask = nmfs[nmfs$area_name == iarea]),
+  #      col = "black", add = TRUE, border = F)
 
   ## Untrawlable areas
-  plot(terra::mask(x = goa_grid_untrawl,
-                   mask = nmfs[nmfs$area_name == iarea]),
-       col = rgb(0, 0, 0, 0.5), add = TRUE, border = FALSE)
+  # plot(terra::mask(x = UT_stations21[UT_stations21],
+  #                  mask = nmfs[nmfs$area_name == iarea]),
+  #      col = rgb(0, 0, 0, 0.5), add = TRUE, border = FALSE)
 
   ## Land
-  plot(ak_land, add = TRUE, col = "tan", border = T, lwd = 0.5)
-  plot(ca_land, add = TRUE, col = "tan", border = T, lwd = 0.5)
+  plot(ak_land, add = TRUE, col = "tan", border = T, lwd = 0.1)
+  plot(ca_land, add = TRUE, col = "tan", border = T, lwd = 0.1)
 
   ## Strata
   plot(strata_list[strata_list$INPFC_AREA == iarea], lwd = 0.05, add = TRUE)
@@ -341,8 +301,14 @@ for (iarea in unique(depth_mods$manage_area)) { ## Loop over area -- start
            "Southeastern" = "bottomleft")[iarea],
          legend = legend_labels,
          title = "Stratum Legend",
-         fill = c(RColorBrewer::brewer.pal("Spectral", n = n_strata - 1),
-                  "purple", rgb(0, 0, 0, 0.5), "black"))
+         fill = c(RColorBrewer::brewer.pal("Set1", n = n_strata - 1),
+                  "cyan", rgb(0, 0, 0, 0.5), "black"))
   mtext(side = 3, line = -2, text = iarea, font = 2, cex = 1.5)
 }  ## Loop over area -- end
 dev.off()
+
+## Open pdf file
+if (file.exists("data/GOA/processed_shapefiles/updated_strata.pdf")) {
+  system('open  data/GOA/processed_shapefiles/updated_strata.pdf')
+}
+
