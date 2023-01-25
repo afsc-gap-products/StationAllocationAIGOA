f81433ed2407fba20ca03bdf40fa871f45f29c3c
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-01-25 12:35:42 -0800

    restored optim)df to data.r

--- R/data.r
+++ R/data.r
@@ -1,4 +1,4 @@
-#' Name_of_the_data
+#' Name_of_the_data1
 #'
 #' Description_of_the_Data
 #'
@@ -8,3 +8,36 @@
 #'   \item{Col2}{description of Col2}
 #' }
 "depth_mods_2023"
+
+#' Name_of_the_data2
+#'
+#' Description_of_the_Data
+#'
+#' @format A data frame with 10000 rows and 2 variables:
+#' \describe{
+#'   \item{Col1}{description of Col1}
+#'   \item{Col2}{description of Col2}
+#' }
+"D_gct"
+
+#' Name_of_the_data3
+#'
+#' Description_of_the_Data
+#'
+#' @format A data frame with 10000 rows and 2 variables:
+#' \describe{
+#'   \item{Col1}{description of Col1}
+#'   \item{Col2}{description of Col2}
+#' }
+"goa_stations_2025"
+
+#' Name_of_the_data4
+#'
+#' Description_of_the_Data
+#'
+#' @format A data frame with 10000 rows and 2 variables:
+#' \describe{
+#'   \item{Col1}{description of Col1}
+#'   \item{Col2}{description of Col2}
+#' }
+"optim_df"
