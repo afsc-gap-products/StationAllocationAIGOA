f91472d0713ad5040d570d55220257a747d7555f
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2024-08-07 14:21:50 -0700

    removed need for reassigning strata to cells

--- R/data.r
+++ R/data.r
@@ -1,32 +1,65 @@
 
-#' Name_of_the_data2
+#' Predicted densities: Gulf of Alaska Groundfish, 1996-present
 #'
-#' Description_of_the_Data
+#' Predicted densities (kg/km2) from VAST for 15 species/complexes from
+#' 1996-present (excluding 2001).
 #'
-#' @format A data frame with 10000 rows and 2 variables:
+#' @format A 3-dimensional array of predicted densities, units kg/km2
 #' \describe{
-#'   \item{Col1}{description of Col1}
-#'   \item{Col2}{description of Col2}
-#"D_gct"
+#'   \item{grid cell}{VAST interpolation grid cell, denoted by subscript g}
+#'   \item{taxon}{species/complex, denoted by subscript c}
+#'   \item{time}{year, denoted by subscript t}
+#'   }
+#'
+"D_gct"
 
-#' Name_of_the_data3
+#' Gulf of Alaska (GOA) Bottom Trawl Survey Stations
 #'
-#' Description_of_the_Data
+#' Includes trawlability information from 2023
 #'
 #' @format A data frame with 10000 rows and 2 variables:
 #' \describe{
-#'   \item{Col1}{description of Col1}
-#'   \item{Col2}{description of Col2}
+#'   \item{GRIDID}{Position of the 5-km grid cell }
+#'   \item{NMFS_AREA}{NMFS statistical area name.}
+#'   \item{REP_AREA}{Numeric code for NMFS statistical areas.}
+#'   \item{STRATUM}{Numeric code for GOA stratum.}
+#'   \item{TRAWLABLE}{Character string denoting trawlability status. Valid
+#'   values: 'Y' for trawlable, 'N' for untrawlable, and 'UNK' for unknown.}
+#'   \item{AREA_KM2}{Numeric, station area in square kilometers}
 #' }
-#"goa_stations_2025"
+#'
+"goa_stations"
 
-#' Name_of_the_data4
+#' VAST interpolation grid
 #'
-#' Description_of_the_Data
+#' data frame of VAST interpolation grid cells used in the optimization of
+#' station effort across Gulf of Alaska bottom trawl survey strata (2025-on).
 #'
-#' @format A data frame with 10000 rows and 2 variables:
+#' @format A data frame with fields:
 #' \describe{
-#'   \item{Col1}{description of Col1}
-#'   \item{Col2}{description of Col2}
+#'   \item{ID}{Integer. Unique cell id.}
+#'   \item{Area_km2}{Numeric. total area of the cell in square kilometers.}
+#'   \item{Depth_m}{Numeric. Depth of the cell in meters}
+#'   \item{Eastings}{Numeric. Eastings in meters using NAD83 / Alaska Albers (EPSG:3338)}
+#'   \item{Northings}{Numeric. Northings in meters using NAD83 / Alaska Albers (EPSG:3338)}
+#'   \item{NMFS_AREA}{NMFS statistical area name.}
+#'   \item{STRATUM}{Numeric code for GOA stratum.}
 #' }
-#"optim_df"
+"optim_df"
+
+#' 2025 Gulf of Alaska Stratum Boundaries
+#'
+#' data frame loop up table of depth stratum boundaries by NMFS management area used for the Gulf of Alaska bottom trawl survey design (2025-on).
+#'
+#' @format A data frame with fields:
+#' \describe{
+#'   \item{NMFS_AREA}{NMFS statistical area name.}
+#'   \item{REP_AREA}{Numeric code for NMFS statistical areas.}
+#'   \item{STRATUM}{Numeric code for GOA stratum.}
+#'   \item{DEPTH_MIN_M}{Numeric. Minimum boundary of the stratum in meters.}
+#'   \item{DEPTH_MAX_M}{Numeric. Maximum boundary of the stratum in meters.}
+#'   \item{AREA_KM2}{Numeric. Total area of the stratum in square kilometers.}
+#'   \item{PERIM_KM}{Numeric. Total perimeter of the stratum in kilometers.}
+#'   \item{USED}{Boolean. Is the stratum currently used in the GOA survey footprint? This field is relevant when considering the GOA survey footprint (up to 700 m or 1000 m?).}
+#' }
+"goa_stratum_boundaries"
--- analysis_scripts/GOA/1E_optimization_data.R
+++ analysis_scripts/GOA/1E_optimization_data.R
@@ -19,12 +19,20 @@ library(terra)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Load the true density, and spatial domain dataset
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-grid_goa <- read.csv(file = "data/GOA/vast_grid_goa.csv")
-D_gct <- readRDS("data/GOA/VAST_fit_D_gct.RDS")
+optim_df <- read.csv(file = "data/GOA/vast_data/goa_interpolation_grid.csv")
 
-updated_goa_strata <-
-  terra::vect(x = "data/GOA/processed_shapefiles/goa_strata_2023.shp")
-depth_mods <- read.csv("data/GOA/strata_boundaries/depth_modifications_2023.csv")
+vast_data <- read.csv(file = "data/GOA/vast_data/goa_data_geostat.csv")
+D_gct <- readRDS(file = "data/GOA/vast_data/VAST_fit_D_gct.RDS")
+dimnames(x = D_gct)[[3]] <- sort(x = unique(x = vast_data$Year))
+
+goa_stratum_boundaries <- as.data.frame(
+  terra::vect(x = "data/GOA/shapefiles_akgfmaps/goa_strata_2025.gpkg")
+)
+goa_stratum_boundaries$USED <- goa_stratum_boundaries$DEPTH_MIN_M != 701
+
+goa_stations <- as.data.frame(
+  terra::vect(x = "data/GOA/shapefiles_akgfmaps/goa_stations_2025.gpkg")
+)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Constants used throughout all scripts
@@ -32,44 +40,14 @@ depth_mods <- read.csv("data/GOA/strata_boundaries/depth_modifications_2023.csv"
 ## crs used
 lonlat_crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 utm_crs <- "+proj=utm +zone=5N +units=km"
-n_years <- dim(D_gct)[3]
-n_spp <- dim(D_gct)[2]
-n_cells <- dim(D_gct)[1]
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Assign grid points to the new strata
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-grid_goa_sp <- terra::vect(x = cbind(ID = 1:nrow(grid_goa), grid_goa),
-                           geom = c("Lon", "Lat"),
-                           crs = lonlat_crs)
-grid_goa_sp <- terra::project(x = grid_goa_sp,
-                              y = updated_goa_strata)
-
-grid_goa_sp <-
-  terra::intersect(x = grid_goa_sp,
-                   y = updated_goa_strata[, c("INPFC_AREA", "STRATUM")])
-grid_goa_sp <- grid_goa_sp[grid_goa_sp$STRATUM %in%
-                             depth_mods$stratum[depth_mods$used], ]
-
-## remove duplicates
-rm_idx <- c()
-
-for (idx in names(which(table(grid_goa_sp$ID) == 2))) {
-  rm_idx <- c(rm_idx, which(grid_goa_sp$ID == idx)[1])
-}
-
-grid_goa_sp <- grid_goa_sp[-rm_idx, ]
-
-removed_cells <- (1:n_cells)[-grid_goa_sp$ID]
-D_gct <- D_gct[-c(removed_cells), , ]
-n_cells <- dim(D_gct)[1]
-
-optim_df <- cbind(as.data.frame(grid_goa_sp),
-                  terra::geom(grid_goa_sp)[, c("x", "y")])
-attributes(optim_df)$crs <- terra::crs(grid_goa_sp)
+n_years <- dim(x = D_gct)[3]
+n_spp <- dim(x = D_gct)[2]
+n_cells <- dim(x = D_gct)[1]
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Save data internally
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 usethis::use_data(D_gct, overwrite = TRUE)
 usethis::use_data(optim_df, overwrite = TRUE)
+usethis::use_data(goa_stations, overwrite = TRUE)
+usethis::use_data(goa_stratum_boundaries, overwrite = TRUE)
