56415b395490d3497150fca1fcc3bd6729b4f13c
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2025-04-12 13:40:51 -0700

    added conversion function

---
+++ R/convert_dd_to_mins.R
@@ -0,0 +1,15 @@
+#' Converts Latitude or Longitude from Decimal Degrees to Minutes?
+#'
+#' @description Station lat/lon are converted from decimal degrees to
+#' minutes for the skipper paper station logs
+#'
+#' @author Zack Oyafuso \email{zack.oyafuso@@noaa.gov}
+#'
+#' @param x vector of latitudes or longitudes to convert.
+#' @param digits integer indicating the number of decimal places to be used.
+#'
+
+convert_dd_to_mins <- function(x, digits = 2) {
+  round(x = (x - floor(x) * 60 / 100) + floor(x) * 100,
+        digits = digits)
+}
