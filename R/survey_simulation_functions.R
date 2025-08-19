0f6bc7d119bebf53bfc4b4f29ba0ceefabdca234
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2025-08-18 21:05:08 -0700

    fixed indexing of strata

--- R/survey_simulation_functions.R
+++ R/survey_simulation_functions.R
@@ -10,18 +10,18 @@
 #' @export
 #'
 sample_srs = function(d_gc = NULL,
-                  sample_size=550){
+                      sample_size=550){
 
   ss1 <- sample(1:nrow(x = d_gc), size = sample_size)
   species_names <- colnames(x = d_gc)
 
   #Calculate the mean of the random sample
   srs_mean <- apply(X = d_gc[ss1, ],
-                  MARGIN = 2,
-                  FUN = mean)
+                    MARGIN = 2,
+                    FUN = mean)
   srs_var <-apply(X = d_gc[ss1, ],
-               MARGIN = 2,
-               FUN = var)
+                  MARGIN = 2,
+                  FUN = var)
 
   #Output: dataframe mean, sd, and CV of random sample for each species
   return(data.frame(species_names, srs_mean, srs_var))
@@ -44,15 +44,14 @@ sample_srs = function(d_gc = NULL,
 #' @export
 #'
 simulate_goa_strs <- function(d_gct = NULL,
-                                grid_df = NULL,
-                                stn_allocation = goa_stn_all) {
+                              grid_df = NULL,
+                              stn_allocation = goa_stn_all) {
 
   n_strata <- nrow(x = stn_allocation)
   n_cells <- nrow(x = grid_df)
   n_species <- dim(x = d_gct)[2]
   n_years <- dim(x = d_gct)[3]
   species_names <- dimnames(x = d_gct)[[2]]
-  strata_names <- stn_allocation$stratum
 
   ## Create a 20 x 6 matrix (WIS, 59x6 for TSRS) with 20 strata.
   ## The 6 cols: NNh,NN,nh,mean,sd,cv PvS 3/25/24
@@ -63,22 +62,30 @@ simulate_goa_strs <- function(d_gct = NULL,
     merge(y = stn_allocation,
           by = "stratum")
 
+  strata_names <- stratum_info$stratum
+
   ## finite-pop'n correction factor
   stratum_info$fin_corr <- with(stratum_info, (NNh - n) / NNh)
 
   mean_stats <- var_stats <-
     array(dim = c(n_strata, n_species, n_years),
-          dimnames = list(stratum_info$stratum, species_names, NULL))
+          dimnames = list(strata_names, species_names, NULL))
 
   for (iyear in 1:n_years) {
-    for (ii in 1:n_strata) { #Loop over strata -- start
+    for (ii in strata_names) { #Loop over strata -- start
+
+      stratum_idx <- grid_df$id[grid_df$STRATUM == ii]
+      # plot(Lat ~ Lon, data = grid_df[grid_df$id %in% stratum_idx, ],
+      # main = paste("Stratum", stratum_info$stratum[ii]),
+      # pch = 16, cex = 0.5)
+
       #For each stratum ii, randomly sample stations based on the allocation
       #and populate "strata.stats" with the SRS mean, sd, CV
       stratum_stats <-
-        sample_srs(d_gct = d_gct[grid_df$STRATUM == stratum_info$stratum[ii],
-                             ,
-                             iyear],
-               sample_size = stratum_info$n[ii])
+        sample_srs(d_gc = d_gct[stratum_idx,
+                                ,
+                                iyear],
+                   sample_size = stratum_info$n[stratum_info$stratum == ii])
       mean_stats[ii, , iyear] <- stratum_stats$srs_mean
       var_stats[ii, , iyear] <- stratum_stats$srs_var
     }
@@ -87,13 +94,13 @@ simulate_goa_strs <- function(d_gct = NULL,
   #Calculate STRS mean, which is a weighted mean across all strata
   mean_all <- apply(X = mean_stats,
                     MARGIN = 2:3,
-                    FUN = function(x) with(stratum_info, sum(x * WWh)))
+                    FUN = function(x) sum(x * stratum_info$WWh))
 
   #Calculate STRS standard error of the mean with finite-pop'n correction factor
   se_all <- apply(X = var_stats,
                   MARGIN = 2:3,
                   FUN = function(x)
-                    with(stratum_info, sqrt(sum(WWh^2*fin_corr*(x)/n)))
+                    sqrt(sum(stratum_info$WWh^2*stratum_info$fin_corr*(x)/stratum_info$n))
   )
 
   ## Calculate STRS CV
