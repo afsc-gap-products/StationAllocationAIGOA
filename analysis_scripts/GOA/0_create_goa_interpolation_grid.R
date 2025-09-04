36852042edbdba9a87c4b6020848655561829985
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2025-09-03 18:13:44 -0700

    attached stratum info to grid cells

--- analysis_scripts/GOA/0_create_goa_interpolation_grid.R
+++ analysis_scripts/GOA/0_create_goa_interpolation_grid.R
@@ -4,9 +4,7 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## Import libraries
-library(akgfmaps)
-library(terra)
-library(sf)
+library(akgfmaps); library(terra); library(sf)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Create 2-nmi grid within the 2025 GOA footprint
@@ -20,12 +18,11 @@ goa_footprint_2025 <-
   )$survey.strata |>
   ## Remove strata in the 700 - 1000 m depth zone
   subset(subset = STRATUM < 500) |>
-  ## Dissolve boundaries
-  terra::vect() |>
-  terra::aggregate()
+  terra::vect()
 
+## Create a 2-nautical-mile (3.704 km) rectangular grid that overlaps with the
+## 2025 GOA survey footprint
 goa_grid <-
-  ## Create a rectangular grid that overlaps with the 2025 GOA footprint
   sf::st_make_grid(x = goa_footprint_2025,
                    square = T,
                    cellsize = 3.704) |>
@@ -33,20 +30,19 @@ goa_grid <-
   ## Intersect with the footprint
   terra::intersect(goa_footprint_2025)
 
-goa_grid_df <- data.frame(terra::centroids(x = goa_grid) |> terra::crds())
-names(x = goa_grid_df) <- c("X", "Y")
+## Attach the centroids (in UTMs)
+goa_grid_df <- data.frame(stratum = goa_grid$STRATUM,
+                          terra::centroids(x = goa_grid) |> terra::crds())
+names(x = goa_grid_df) <- toupper(x = names(x = goa_grid_df))
 
-goa_grid_df[, c("lon", "lat")] <-
+## Project sampling unit centroids in lat/lon
+goa_grid_df[, c("LON", "LAT")] <-
   terra::project(x = goa_grid, "+proj=longlat +datum=WGS84") |>
   terra::centroids() |>
   terra::crds()
 
-goa_grid_df$area_km2 <- terra::expanse(x = goa_grid) / 1e6
-
-## Check to make sure the total area of the interpolation grid matches the
-## actual survey footprint.
-sum(goa_grid_df$area_km2) # 301174.5 km2
-expanse(goa_footprint_2025) * 1e-6 # 301174.7 km2
+## Calculate area of each sampling unit
+goa_grid_df$AREA_KM2 <- terra::expanse(x = goa_grid) / 1e6
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Write to file
