5a18e0ac2aca12c3c8b8c767b9549886c06c32df
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-01-26 12:15:32 -0800

    combine sql calls

--- R/get.ai.stations.R
+++ R/get.ai.stations.R
@@ -27,78 +27,11 @@ get.ai.stations <- function(channel = NULL) {
   ## Connect to Oracle if not already
   if(is.na(x = channel)) channel <- gapindex::get_connected()
 
-  TEMPSTNS <- RODBC::sqlQuery(
+  dat <- RODBC::sqlQuery(
     channel = channel,
     query = read_sql(filepath =
                        system.file("SQL/get_ai_stations.sql",
                                    package = "StationAllocationAIGOA")))
-
-  cat("\nPopulating candidate station list...\n")
-  ## Create a new TEMPSTNS table with sqlSave
-  RODBC::sqlSave(channel = channel,
-                 dat = TEMPSTNS,
-                 rownames = F,
-                 fast = F,
-                 nastring = NULL)
-
-  # Query to get area and position of all station segments for all
-  # candidate stations
-  GRDPTS <- RODBC::sqlQuery(channel = channel,
-                            query = "SELECT
-                                     A.STATIONID,
-                                     A.STRATUM,
-                                     TO_NUMBER(B.CENTER_LONG) LONGITUDE,
-	                                   TO_NUMBER(B.CENTER_LAT) LATITUDE,
-	                                   TO_NUMBER(B.AREA_KM2) AREA
-	                                   FROM
-	                                   TEMPSTNS A,
-	                                   AI.AIGRID_GIS B
-	                                   WHERE A.STATIONID = B.STATIONID
-	                                   AND A.STRATUM = TO_NUMBER(B.STRATUM)")
-
-  # Create a new GRDPTS table in Oracle with sqlSave
-  RODBC::sqlSave(channel = channel,
-                 dat = GRDPTS,
-                 rownames = F,
-                 fast = F,
-                 nastring = NULL)
-
-  # Query to get largest continuous area within a station for assigning the
-  # station centroid
-  MAXAREA <- RODBC::sqlQuery(channel = channel,
-                             query = "SELECT
-                                      STATIONID,
-                                      STRATUM,
-                                      MAX(AREA) MAX_AREA
-                                      FROM GRDPTS
-                                      GROUP BY STATIONID, STRATUM")
-
-  # Create a new MAXAREA table in Oracle with sqlSave
-  RODBC::sqlSave(channel = channel,
-                 dat = MAXAREA,
-                 rownames = F,
-                 fast = F,
-                 nastring = NULL)
-
-  cat("\nAssigning coordinates for station centroids...\n")
-  # Assign station centroids to stations identified by stationid & stratum
-  dat <- RODBC::sqlQuery(channel = channel,
-                         query =  "SELECT
-                                   A.STATIONID,
-                                   A.STRATUM,
-                                   LONGITUDE,
-                                   LATITUDE
-                                   FROM GRDPTS A,
-                                   MAXAREA B
-                                   WHERE A.STATIONID = B.STATIONID
-                                   AND A.STRATUM = B.STRATUM
-                                   AND NVL(AREA,-1) = NVL(MAX_AREA, -1)")
-  names(dat) <- casefold(names(x = dat))
-
-  # clean up by dropping temporary tables
-  RODBC::sqlDrop(channel, "TEMPSTNS")
-  RODBC::sqlDrop(channel, "GRDPTS")
-  RODBC::sqlDrop(channel, "MAXAREA")
 
   return(dat)
 
